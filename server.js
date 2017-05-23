efd596a08ba64cf771cf9808db330e966c35422a
CEllis <tehwavy@gmail.com>
2017-05-24 02:45:56 +1000

    Added body-parser and useragent dependencies - finished parser functionality

--- server.js
+++ server.js
@@ -1,9 +1,31 @@
+// Set up dependencies
 var express = require('express');
-var app = express();
+var bodyParser = require('body-parser');
+var useragent = require('express-useragent');
+
+// Define port
 var port = process.env.PORT || 3000;
 
-app.use(express.static('public'));
+// Configure server
+var app = express();
+app.use(express.static('./public'));
+app.use(bodyParser.json());
+app.use(useragent.express());
 
+// Set up routes
+app.get('/api', function (req, res) {
+  console.log(req);
+  var header = req.headers;
+  var language = header['accept-language'].split(',');
+
+  res.json({
+    ipaddress: header.host,
+    language: language[0],
+    software: req.useragent.platform + ', ' + req.useragent.os
+  });
+});
+
+// Start server
 app.listen(port, function() {
-  console.log('App is running on http://localhost:' + port);
+  console.log('App is running on port ' + port);
 });
